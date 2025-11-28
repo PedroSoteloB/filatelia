7fb9d064903cb6377b5aaebe34de2ae1983f31bc
PedroSoteloB <113255967+PedroSoteloB@users.noreply.github.com>
2025-11-27 21:15:19 -0500

    Change apiBaseUrl to local backend URL

--- frontend/src/app/core/environments/environment.ts
+++ frontend/src/app/core/environments/environment.ts
@@ -1,7 +1,4 @@
-// src/environments/environment.ts
 export const environment = {
   production: false,
-  // Si quieres usar el backend de Render también en dev, deja esto:
-  apiBaseUrl: 'https://filatelia.onrender.com',
-  // Si quisieras usar back local, sería: 'http://localhost:3000'
+  apiBaseUrl: 'http://localhost:3000', // back local
 };
