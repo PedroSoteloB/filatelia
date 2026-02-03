5f337fe808fa66e1e3ba442e58f0567640f14002
PedroSoteloB <113255967+PedroSoteloB@users.noreply.github.com>
2026-02-02 23:16:10 -0500

    c

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,61 +1,3 @@
-// import { Routes } from '@angular/router';
-// import { adminGuard } from './core/guards/admin.guard';
-
-// export const routes: Routes = [
-//   // Landing pública como raíz
-//   {
-//     path: '',
-//     loadComponent: () =>
-//       import('./features/site/landing/landing.component')
-//         .then(m => m.LandingComponent)
-//   },
-
-//   {
-//     path: 'login',
-//     loadComponent: () =>
-//       import('./features/auth/login/login.component').then(m => m.LoginComponent)
-//   },
-//   {
-//     path: 'forgot-password',
-//     loadComponent: () =>
-//       import('./features/auth/forgot-password/forgot-password.component')
-//         .then(m => m.ForgotPasswordComponent)
-//   },
-//   {
-//     path: 'reset-password',
-//     loadComponent: () =>
-//       import('./features/auth/reset-password/reset-password.component')
-//         .then(m => m.ResetPasswordComponent)
-//   },
-
-//   // Admin (protegido por rol)
-//   {
-//     path: 'admin',
-//     canMatch: [adminGuard],
-//     loadComponent: () =>
-//       import('./features/admin/admin-landing/landing/landing.component')
-//         .then(m => m.LandingComponent)
-//   },
-  
-//   {
-//     path: 'admin/roles',
-//     canMatch: [adminGuard],
-//     loadComponent: () =>
-//       import('./features/admin/roles/roles.component')
-//         .then(m => m.AdminRolesComponent)
-//   },
-
-//   {
-//     path: 'app/new',
-//     loadComponent: () =>
-//       import('./features/site/landing/upload-item/upload-item.component')
-//         .then(m => m.UploadItemComponent)
-//   }
-  
-
-  
-// ];
-
 import { Routes } from '@angular/router';
 import { adminGuard } from './core/guards/admin.guard';
 
@@ -109,6 +51,12 @@ export const routes: Routes = [
       import('./features/site/functions/list/my-items.component')
         .then(m => m.MyItemsComponent)
   } ,
+  {
+    path: 'items/:id', // ✅ AGREGA ESTO
+    loadComponent: () =>
+      import('./features/site/functions/item-details/item-details.component')
+        .then(m => m.ItemDetailsComponent)
+  },
   {
     path: 'items/mine/:id',
     loadComponent: () =>
